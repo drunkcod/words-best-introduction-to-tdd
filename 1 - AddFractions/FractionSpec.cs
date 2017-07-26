c04b58ba40a03c39ee77b680695032ea3bd17999
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-07-26 16:19:25 +0200

    Add Zeros

--- 1 - AddFractions/FractionSpec.cs
+++ 1 - AddFractions/FractionSpec.cs
@@ -2,11 +2,19 @@
 
 namespace AddFractions
 {
-	class Fraction { }
+	struct Fraction
+	{
+		public Fraction(int n, int d) { }
+
+		public static Fraction operator+(Fraction lhs, Fraction rhs) => new Fraction();
+
+		public static bool operator==(Fraction lhs, Fraction rhs) => true;
+		public static bool operator!=(Fraction lhs, Fraction rhs) => !(lhs == rhs);
+	}
 
 	[Describe(typeof(Fraction))]
     public class FractionSpec
     {
-		public void all_is_good() { }
+		public void adding_zeros_yields_zero() => Check.That(() => new Fraction(0, 1) + new Fraction(0, 2) == new Fraction(0, 3));
     }
 }
