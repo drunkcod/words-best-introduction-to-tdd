01b159d6a9615a10ab3ae0cf49a1adbbd1853929
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-07-26 16:24:39 +0200

    add zero to something

--- 1 - AddFractions/FractionSpec.cs
+++ 1 - AddFractions/FractionSpec.cs
@@ -4,17 +4,26 @@ namespace AddFractions
 {
 	struct Fraction
 	{
-		public Fraction(int n, int d) { }
+		readonly int n;
+		public Fraction(int n) { this.n = n;}
 
-		public static Fraction operator+(Fraction lhs, Fraction rhs) => new Fraction();
+		public static Fraction operator+(Fraction lhs, Fraction rhs) => lhs;
 
-		public static bool operator==(Fraction lhs, Fraction rhs) => true;
+		public static bool operator==(Fraction lhs, Fraction rhs) => lhs.n == rhs.n;
 		public static bool operator!=(Fraction lhs, Fraction rhs) => !(lhs == rhs);
+
+		public override string ToString() => n.ToString();
 	}
 
 	[Describe(typeof(Fraction))]
     public class FractionSpec
     {
-		public void adding_zeros_yields_zero() => Check.That(() => new Fraction(0, 1) + new Fraction(0, 2) == new Fraction(0, 3));
+		public void adding_zeros_yields_zero() => 
+			Check.That(() => new Fraction(0) + new Fraction(0) == new Fraction(0));
+
+		public void add_zero_to_something() {
+			var something = new Fraction(3);
+			Check.That(() => something + new Fraction(0) == something);
+		}
     }
 }
