c2bf33b17ac22f137a05d0049c403574493cffda
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-07-31 19:36:12 +0200

    ctor logic moved into factory function.

--- 1 - AddFractions/FractionSpec.cs
+++ 1 - AddFractions/FractionSpec.cs
@@ -5,18 +5,17 @@ namespace AddFractions
 {
 	struct Fraction
 	{
-		public Fraction(int n) : this(n, 1) { }
-
 		public Fraction(int n, int d) {
-			if(d == 0) throw new ArgumentOutOfRangeException(nameof(d));
-			if(d > 0) {
-				this.Numerator = n;
-				this.Denominator = d;
-			} else {
-				this.Numerator = -n;
-				this.Denominator = -d;
-			}
+			if(d <= 0) throw new ArgumentOutOfRangeException(nameof(d));
+
+			this.Numerator = n;
+			this.Denominator = d;
 		}
+
+		public static Fraction Create(int numerator) => Create(numerator, 1);
+		public static Fraction Create(int numerator, int denominator) =>
+			(denominator > 0 ? new Fraction(numerator, denominator) : new Fraction(-numerator, -denominator))
+			.Reduce();
 
 		public readonly int Numerator;
 		public readonly int Denominator;
@@ -46,46 +45,20 @@ namespace AddFractions
 				b = a % b;
 				a = t;
 			}
-			return a;
+			return Math.Abs(a);
 		}
 	}
 
 	[Describe(typeof(Fraction))]
     public class FractionSpec
     {
-		public void adding_zeros_yields_zero() => 
-			Check.That(() => new Fraction(0) + new Fraction(0) == new Fraction(0));
-
-		public void add_zero_to_something() {
-			var something = new Fraction(3);
-			Check.That(() => something + new Fraction(0) == something);
-		}
-
-		public void add_something_to_zero() {
-			var something = new Fraction(5);
-			Check.That(() => new Fraction(0) + something == something);
-		}
-
-		public void add_something_to_something_else() =>
-			Check.That(() => new Fraction(-3) + new Fraction(1) == new Fraction(-2));
-
-		public void add_with_common_denominator() =>
-			Check.With(() => new Fraction(1, 3) + new Fraction(1, 3)).That(
-				sum => sum == new Fraction(2, 3),
-				sum => sum.Numerator == 2,
-				sum => sum.Denominator == 3);
-
-		public void add_with_different_denomoniators() =>
-			Check.That(() => new Fraction(1, 2) + new Fraction(2, 5) == new Fraction(9, 10));
-
 		public void denominator_cant_be_zero() => Check.Exception<ArgumentException>(() => new Fraction(1, 0));
-
-		public void equality_checks_denomniator() => Check.That(() => !(new Fraction(1, 2) == new Fraction(1, 3)));
+		public void denominator_cant_be_sub_zero() => Check.Exception<ArgumentException>(() => new Fraction(1, -1));
 
 		public void reduce() =>
 			Check.That(
-				() => new Fraction(4, 4).Reduce() == new Fraction(1),
-				() => new Fraction(4, 2).Reduce() == new Fraction(2),
+				() => new Fraction(4, 4).Reduce() == Fraction.Create(1),
+				() => new Fraction(4, 2).Reduce() == Fraction.Create(2),
 				() => new Fraction(2, 4).Reduce() == new Fraction(1, 2));
 
 		[DisplayAs("gcd({0}, {1}) == {2}")]
@@ -94,15 +67,50 @@ namespace AddFractions
 		[Row(0, 7, 7)]
 		[Row(0, 1, 1)]
 		[Row(-3, 1, 1)]
+		[Row(3, -1, 1)]
 		public void gcd(int a, int b, int result) => Check.That(() => Fraction.Gcd(a, b) == result);
 
-		public void add_reduces_result() => Check.That(() => new Fraction(1, 4) + new Fraction(1, 4) == new Fraction(1, 2));
+		public void create_reduces() => Check.With(() => Fraction.Create(3, 6))
+			.That(x => x.Numerator == 1, x => x.Denominator == 2);
 
 		public void negative_fractions_have_positive_denominators() =>
-			Check.With(() => new Fraction(1, -7)).That(
+			Check.With(() => Fraction.Create(1, -7)).That(
 				x => x.Numerator == -1,
 				x => x.Denominator == 7);
 
+		public void equality() => Check.That(
+			() => new Fraction(1, 4) == new Fraction(2, 8),
+			() => new Fraction(1, 4) != new Fraction(4, 1));
+
 		public void equality_normalized_denominators() => Check.That(() => new Fraction(2, 4) == new Fraction(1, 2));
+
+		public void equality_checks_denomniator() => Check.That(() => !(new Fraction(1, 2) == new Fraction(1, 3)));
     }
+
+	[Describe(typeof(Fraction), "adding")]
+	public class FractionAddSpec
+	{
+		public void zeros_and_zero_yields_zero() => 
+			Check.That(() => Fraction.Create(0) + Fraction.Create(0) == Fraction.Create(0));
+
+		public void zero_to_something() => Check.With(() => Fraction.Create(3))
+			.That(something => something + Fraction.Create(0) == something);
+
+		public void something_to_zero() => Check.With(() => Fraction.Create(5))
+			.That(something => Fraction.Create(0) + something == something);
+
+		public void something_to_something_else() =>
+			Check.That(() => Fraction.Create(-3) + Fraction.Create(1) == Fraction.Create(-2));
+
+		public void with_common_denominator() =>
+			Check.With(() => new Fraction(1, 3) + new Fraction(1, 3)).That(
+				sum => sum == new Fraction(2, 3),
+				sum => sum.Numerator == 2,
+				sum => sum.Denominator == 3);
+
+		public void with_different_denomoniators() =>
+			Check.That(() => new Fraction(1, 2) + new Fraction(2, 5) == new Fraction(9, 10));
+
+		public void reduces_result() => Check.That(() => new Fraction(1, 4) + new Fraction(1, 4) == new Fraction(1, 2));
+	}
 }
