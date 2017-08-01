e5424a81928fe14ad72dff3555780f01c56652b0
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-08-01 17:16:34 +0200

    Removed duplication.

--- 2 - Point of Sale Exercise/Display.cs
+++ 2 - Point of Sale Exercise/Display.cs
@@ -7,13 +7,15 @@ namespace PointOfSale
 		public string Text { get; private set; }
 
 		public void DisplayError(string errorMessage) => Text = errorMessage;
-		public void DisplayTotal(Price totalPrice) => Text = string.Format("Total: ${0}", totalPrice.ToString(CultureInfo.InvariantCulture));
-		void DisplayPrice(Price itemPrice) => Text = string.Format("${0}", itemPrice.ToString(CultureInfo.InvariantCulture));
+		public void DisplayTotal(Price totalPrice) => Text = $"Total: {Format(totalPrice)}";
+		void DisplayPrice(Price itemPrice) => Text = Format(itemPrice);
 		void DisplayMissingProduct(Barcode missingItem) => Text = $"Missing Product <{missingItem}>";
 
 		public void ConnectTo(PosTerminal terminal) {
 			terminal.ItemAdded += (_, e) => DisplayPrice(e.ItemPrice);
 			terminal.MissingItem += (_, e) => DisplayMissingProduct(e.Barcode);
 		}
+
+		static string Format(Price price) => string.Format("${0}", price.ToString(CultureInfo.InvariantCulture));
 	}
 }
