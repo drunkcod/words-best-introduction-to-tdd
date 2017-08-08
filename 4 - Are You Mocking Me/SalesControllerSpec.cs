4f5541ace112e2287917f564c929269183436c21
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-08-08 23:28:39 +0200

    Sell a thing having a price, any price. Just stubs.

--- 4 - Are You Mocking Me/SalesControllerSpec.cs
+++ 4 - Are You Mocking Me/SalesControllerSpec.cs
@@ -1,18 +1,77 @@
 ﻿using Cone;
+using Cone.Helpers;
+using PointOfSale;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClientFirstWithMocks
 {
 	class SalesController
-	{ }
+	{ 
+		readonly ICatalog catalog;
+		readonly IDisplay display;
+
+		public SalesController(ICatalog catalog, IDisplay display) {
+			this.catalog = catalog;
+			this.display = display;
+		}
+
+		public void OnBarcode(Barcode barcode) { 
+			var price = catalog.GetPrice(barcode);
+			if(price.HasValue)
+				display.DisplayTotal(price.Value);
+		}
+	}
+
+	interface ICatalog
+	{
+		Price? GetPrice(Barcode barcode);
+	}
+
+	interface IDisplay
+	{ 
+		void DisplayTotal(Price price);
+	}
+
+	class DisplayStub : IDisplay
+	{ 
+		public Action<Price> DisplayTotal;
+
+		void IDisplay.DisplayTotal(Price price) => DisplayTotal?.Invoke(price);
+	}
+
+	class CatalogStub : ICatalog
+	{
+		public Func<Barcode, Price?> GetPrice;
+		
+		Price? ICatalog.GetPrice(Barcode barcode) => GetPrice?.Invoke(barcode);
+	}
+
 
 	[Describe(typeof(SalesController))]
     public class SalesControllerSpec
     {
-		public void all_is_well() { }
+		public void product_found() {
+			var expectedPrice = new Price(7.95m);
+			var theBarcode = new Barcode("12345");
+
+			var display = new DisplayStub();
+			var catalog = new CatalogStub {
+				GetPrice = barcode => {
+					Check.That(() => barcode == theBarcode);
+					return expectedPrice;
+				}
+			};
+
+			var displayTotal = MethodSpy.On(ref display.DisplayTotal);
+
+			var sales = new SalesController(catalog, display);
+			sales.OnBarcode(theBarcode);
+
+			Check.That(() => displayTotal.HasBeenCalled);
+			displayTotal.Then(price => Check.That(() => price == expectedPrice));
+		}
+
+		//public void product_missing() { }
+		//public void empty() { }
     }
 }
