11038e1dba28841eb1b0ab6c3a19efc758246b86
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-08-08 22:42:09 +0200

    Starting the mockfest.

---
+++ 4 - Are You Mocking Me/SalesControllerSpec.cs
@@ -0,0 +1,18 @@
+﻿using Cone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientFirstWithMocks
+{
+	class SalesController
+	{ }
+
+	[Describe(typeof(SalesController))]
+    public class SalesControllerSpec
+    {
+		public void all_is_well() { }
+    }
+}
--- Run-Tests.bat
+++ Run-Tests.bat
@@ -1,3 +1,4 @@
 @echo off
 REM Build\packages\Cone.Conesole.2017.7.31.1\tools\net452\Conesole.exe "Build\Debug\1 - AddFractions\AddFractions.dll" %*
-Build\packages\Cone.Conesole.2017.7.31.1\tools\net452\Conesole.exe "Build\Debug\2 - Point of Sale Exercise\PointOfSale.dll" %*
+REM Build\packages\Cone.Conesole.2017.7.31.1\tools\net452\Conesole.exe "Build\Debug\2 - Point of Sale Exercise\PointOfSale.dll" %*
+Build\packages\Cone.Conesole.2017.7.31.1\tools\net452\Conesole.exe "Build\Debug\4 - Are You Mocking Me\ClientFirstWithMocks.dll" %*
